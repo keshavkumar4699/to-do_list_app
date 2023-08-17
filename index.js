4bf6eaf87bad85eb9aff9ca85d81e58c481fa699
Keshav kumar <kumar.keshav4699@gmail.com>
2023-08-17 11:25:25 +0530

    First Commit: create the directory structure and set up express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const port = 8000;
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`server fat gya bhai: ${err}`);//this is called interpolation
+    }
+    console.log(`${port} pe chal rha server`);
+})
